e6f2c9df867857dc64094cff8c1f613690b43904
femi <cocofemi@gmail.com>
2022-07-18 23:43:04 +0100

    Heroku

--- server/serviceAccountKey.js
+++ server/serviceAccountKey.js
@@ -2,7 +2,7 @@ const dotenv = require('./dotenvConfig')()
 
 module.exports = {
   "project_id": process.env.PROJECT_ID,
-  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "private_key": process.env.PRIVATE_KEY,
   "client_email": process.env.CLIENT_EMAIL,
 }
 
