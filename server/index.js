5122c8aa0022106d5e1af9d343d3af16cbb0738e
femi <cocofemi@gmail.com>
2022-07-19 15:13:52 +0100

    Heroku

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(Cors(corsOption))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.resolve(__dirname, "./client/build")))
+// app.use(express.static(path.resolve(__dirname, "./client/build")))
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
--- server/serviceAccountKey.js
+++ server/serviceAccountKey.js
@@ -1,11 +1,8 @@
 const dotenv = require('./dotenvConfig')()
 
-const firebase_private_key_b64 = Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64');
-const firebase_private_key = firebase_private_key_b64.toString('utf8');
-
 module.exports = {
   "project_id": process.env.PROJECT_ID,
-  "private_key": firebase_private_key,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
   "client_email": process.env.CLIENT_EMAIL,
 }
 
