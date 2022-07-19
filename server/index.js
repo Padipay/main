ab6fec4eaf4e2978d6b741ef877a5046b9296b56
femi <cocofemi@gmail.com>
2022-07-19 07:58:41 +0100

    Heroku

--- server/index.js
+++ server/index.js
@@ -20,12 +20,12 @@ const client = require('twilio')(
 );
 
 require("dotenv").config();
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 // initialize Firebase Admin SDK
 const adminApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 })
-
 
 const corsOption = {
   origin: '*',
